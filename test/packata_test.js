fa8062aedc974fa73940707e70178821459b10c9
Javed Ahmed <javedahmed@Javeds-MacBook-Pro.local>
2026-02-10 14:44:49 +0530

    add setup  * make a src and test directory and init the task

---
+++ test/packata_test.js
@@ -0,0 +1,8 @@
+import { assertEquals } from "@std/assert";
+import { describe, it } from "@std/testing";
+
+describe("simple test", () => {
+  it("testing", () => {
+    assertEquals(1, 1);
+  });
+});
