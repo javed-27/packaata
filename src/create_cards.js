f75459e4a0d1b08517dff12411c75df9c67f41ec
Gopi Vinay <gopivinay@Gopis-MacBook-Pro.local>
2026-02-10 15:32:08 +0530

    creates the deck cards.
    * creates number of deck cards.
    * adds joker to the deck.

---
+++ src/create_cards.js
@@ -0,0 +1,24 @@
+export const createSet = (suit, color, values) => 
+  values.map(value => ({value, suit, color}));
+
+export const addJoker = (deck) => 
+  deck.push({value : '0', color : null, suit : null});
+
+export const createDeck = (suits, colors, values) => {
+  const deck = [];
+  for (let index = 0; index < colors.length; index++) {
+    const set = createSet(suits[index], colors[index], values);
+    deck.push(...set);
+  }
+  addJoker(deck);
+  addJoker(deck);
+  return deck;
+}
+
+export const createCards = (noOfDecks = 1) => {
+  const suits = [ 'heart', 'diamond', 'spade','club'];
+  const colors = ['red','red','black','black'];
+  const values = ['A','2','3','4','5','6','7','8','9','10','J','Q','K'];
+  const deck = createDeck(suits, colors, values);
+  return Array.from({length : noOfDecks}).flatMap(() => deck);
+}
---
+++ test/create_cards_test.js
@@ -0,0 +1,171 @@
+import { assertEquals } from "@std/assert";
+import { describe, it } from "@std/testing";
+import { addJoker, createCards, createDeck, createSet } from "../src/create_cards.js";
+
+describe("creating deck cards", () => {
+  it("1. creating a single deck", () => {
+    const deck = createCards();
+    assertEquals(deck.length, 54);
+  });
+
+  it('2. creating 2 deck of cards', () => {
+    const deck = createCards(2);
+    assertEquals(deck.length, 108);
+  })
+
+   it('3. creating 5 deck of cards', () => {
+    const deck = createCards(5);
+    assertEquals(deck.length, 54 * 5);
+  })
+});
+
+describe("creating single set", () => {
+  const values = ["1", "2", "3"];
+  it("1 .creating a heart deck", () => {
+    const set = createSet("heart", "red", values);
+    assertEquals(set.length, 3);
+  });
+
+  it("2. creating a heart deck", () => {
+    const set = createSet("heart", "red", values);
+    const expectedResult = [
+      {
+        color: "red",
+        suit: "heart",
+        value: "1",
+      },
+      {
+        color: "red",
+        suit: "heart",
+        value: "2",
+      },
+      {
+        color: "red",
+        suit: "heart",
+        value: "3",
+      },
+    ];
+    assertEquals(set, expectedResult);
+  });
+
+  it("3. creating the club set", () => {
+    const values = ["J", "Q", "K"];
+    const set = createSet("club", "black", values);
+    const expectedResult = [
+      {
+        color: "black",
+        suit: "club",
+        value: "J",
+      },
+      {
+        color: "black",
+        suit: "club",
+        value: "Q",
+      },
+      {
+        color: "black",
+        suit: "club",
+        value: "K",
+      },
+    ];
+    assertEquals(set, expectedResult);
+  });
+});
+
+describe('adding joker to the set', () => {
+  it('1. empty deck', () =>{
+    const deck = [];
+    addJoker(deck);
+    assertEquals(deck, [{value : '0', color : null, suit : null}]);
+  })
+
+  it('2. adding the addjoker to the deck with cards', () => {
+     const values = ["J", "Q", "K"];
+    const set = createSet("club", "black", values);
+    addJoker(set);
+    const expectedResult = [
+      {
+        color: "black",
+        suit: "club",
+        value: "J",
+      },
+      {
+        color: "black",
+        suit: "club",
+        value: "Q",
+      },
+      {
+        color: "black",
+        suit: "club",
+        value: "K",
+      },
+      {
+        color : null,
+        suit : null,
+        value : '0',
+      }
+    ];
+
+    assertEquals(set, expectedResult);
+  })
+});
+
+describe('creating the deck', () => {
+  it('1. no values , colors, values', () => {
+    const deck = createDeck([],[],[]);
+    const expectedResult = [
+      {
+        color : null,
+        suit : null,
+        value : '0',
+      },
+      {
+        color : null,
+        suit : null,
+        value : '0',
+      }
+    ];
+    assertEquals(deck, expectedResult);
+  })
+
+  it('2. creating the ace card for the all the colors', () => {
+    const suits = ['club', 'heart', 'spade', 'diamond'];
+    const values = ['A'];
+    const colors = ['red','red','red','red'];
+
+    const deck = createDeck(suits, colors, values);
+    const expectedResult = [
+      {
+        color : 'red',
+        suit : 'club',
+        value : 'A',
+      },
+      {
+        color : 'red',
+        suit : 'heart',
+        value : 'A',
+      },
+      {
+        color : 'red',
+        suit : 'spade',
+        value : 'A',
+      },
+      {
+        color : 'red',
+        suit : 'diamond',
+        value : 'A',
+      },
+      {
+        color : null,
+        suit : null,
+        value : '0',
+      },
+      {
+        color : null,
+        suit : null,
+        value : '0',
+      }
+    ];
+    assertEquals(deck, expectedResult);
+  })
+})
