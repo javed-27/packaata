68dbbd884c71f40f8ef33b0f76b6c6fc1e1844f2
Gopi Vinay <gopivinay@Gopis-MacBook-Pro.local>
2026-02-11 17:08:37 +0530

    converts the mouse clicks into the cards position.

--- src/read_positions.js
+++ src/read_positions.js
@@ -14,8 +14,17 @@ export const readPositions = async() => {
   const writer = Deno.stdout.writable.getWriter();
   const encoder = new TextEncoder();
   await writer.write(encoder.encode('\x1b[?1000h'));
-  await readMouseClick(reader, writer);
+  const [col ,row ] = await readMouseClick(reader);
   await writer.write(encoder.encode('\x1b[?1000l'));
+  return [col ,row];
 }
 
-readPositions()
+// readPositions()
+
+const selectCards = async() => {
+  const [col, row] = await readPositions();
+  const index = Math.floor(col / 10); 
+  console.log(index);
+}
+
+selectCards()
